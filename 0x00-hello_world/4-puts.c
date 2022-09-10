1e46105d712072bc5a4453ee068f2cf1e6af3016
Princehinnex <ainokoinnocent1997@gmail.com>
2022-09-10 06:12:07 -0700

    Alone

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,4 @@
 #include <stdio.h>
-
-
-
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
 int main(void)
 
 {
@@ -19,5 +6,4 @@ int main(void)
 		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
-
 }
