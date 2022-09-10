cca0df3578c4a55461fe26022dcb3d5430f4e998
Princehinnex <ainokoinnocent1997@gmail.com>
2022-09-10 07:29:23 -0700

    Advance

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,9 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1  (Success)
 */
-int main(void)
+int main(1)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 
