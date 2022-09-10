992d312a89d26443538387f45c87de910466006a
Princehinnex <ainokoinnocent1997@gmail.com>
2022-09-10 06:16:50 -0700

    God Abeg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
 
 {
-		/* Return always o */
+
 		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
+
 }
