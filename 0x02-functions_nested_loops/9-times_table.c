7061c87672130a83ffe97e7859b5f48604ec0b31
Princehinnex <ainokoinnocent1997@gmail.com>
2022-09-13 21:50:57 -0700

    Done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,7 +4,7 @@
 *
 * Return: Always 0.
 */
-void times_table(void) 
+void times_table(void)
 {
 int a;
 int b;
@@ -14,12 +14,12 @@ for (a = 0; a <= 9; a++)
 for (b = 0; b <= 9; b++)	
 {
 c = a * b;
-if ((c / 10) == 0)   
-{	
+if ((c / 10) == 0)
+{
 if (b == 0)
 {
 _putchar ('0');
-}	      
+}
 if (b != 0)		
 {
 _putchar(' ');
