a496d072ad2fd40d21273f167db4d0f335426c8a
Princehinnex <ainokoinnocent1997@gmail.com>
2022-09-13 21:33:12 -0700

    Done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,4 @@
-#include "main"
+#include "main.h"
 /**
 * jack_bauer - prints every minute of the day
 *
@@ -16,37 +16,11 @@ for (m = 0; m < 60; m++)
 k = m / 10;
 l = m % 10;
 _putchar('0' + i);
-_putchar('0' + j);
-_putchar(':');
-_putchar('0' + k);	  
-_putchar('0' + l);
-_putchar('\n');
-}
-}
-}
-#include "holberton.h"
-/**
-* jack_bauer - prints every minute of the day
-*
-* Return: void
-*/
-void jack_bauer(void)
-{
-int h, m, i, j, k, l;
-for (h = 0; h < 24; h++)
-{
-i = h / 10;
-j = h % 10;
-for (m = 0; m < 60; m++)
-{
-k = m / 10;
-l = m % 10;
-_putchar('0' + i);	  
-_putchar('0' + j);
+_putchar('0' + j);  
 _putchar(':');
 _putchar('0' + k);
 _putchar('0' + l);
 _putchar('\n');
 }
-}
+}  
 }
