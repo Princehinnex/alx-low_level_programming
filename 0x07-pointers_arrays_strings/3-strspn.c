cb044396702ce439f8c470c938ddc6b91a0dbd9c
Princehinnex <ainokoinnocent1997@gmail.com>
2022-09-27 11:19:26 -0700

    Love

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,14 +5,13 @@
 * @accept: This is the string containing the list of characters to match in s
 * Return: return count
 **/
-#unsigned int _strspn(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
 int i, j;
 int count = 0;
 char *str1, *str2;
 str1 = s;
 str2 = accept;
-
 i = 0;
 while (str1[i] != '\0') /*Declaring WHILE *s */
 {
