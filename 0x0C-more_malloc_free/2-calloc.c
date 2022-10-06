2c5fa2421e43c848892020c6f890f7dd2dfe6744
Princehinnex <ainokoinnocent1997@gmail.com>
2022-10-06 13:50:26 -0700

    Kite

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -7,13 +7,13 @@
 * Return: string
 */
 char *_memset(char *s, char b, unsigned int n)
-{  
-unsigned int i;  
-for (i = 0; i < n; i++)    
-{      
-s[i] = b;      
-} 
-return (s); 
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+return (s);
 }
 /**
 * _calloc - allocates memory for an array using malloc
@@ -23,12 +23,12 @@ return (s);
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-void *p; 
-if (nmemb == 0 || size == 0)    
+  void *p;
+if (nmemb == 0 || size == 0)
 return (NULL);
 p = malloc(nmemb * size);
-if (p == NULL)   
-return (NULL); 
-_memset(p, 0, (nmemb * size)); 
-return (p); 
+if (p == NULL)
+return (NULL);
+_memset(p, 0, (nmemb * size));
+return (p);
 }
